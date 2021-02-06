31a03f1a0e4497c0624974e0d976ae64fccbde02
nguyen-t <tuanmng1999@gmail.com>
2021-02-06 04:16:47 -0600

    Changed product lineup

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ let callbacks = [handler.consolePrint, handler.toText(messenger)];
 
 /* Adorama and B&H need captcha handlers*/
 
-// Stocker.Adorama(products.adorama, callbacks);
-Stocker.Amazon(products.amazon, callbacks);
+Stocker.Adorama(products.adorama, callbacks);
+// Stocker.Amazon(products.amazon, callbacks);
 Stocker.BestBuy(products.bestbuy, callbacks);
 // Stocker.BnH(products.bnh, callbacks);
-Stocker.Newegg(products.newegg, callbacks);
+// Stocker.Newegg(products.newegg, callbacks);
 Stocker.OfficeDepot(products.officedepot, callbacks);
