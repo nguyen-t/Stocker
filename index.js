2ad43283c62aa6ed704a89362cdec2f5998d7e3a
nguyen-t <tuanmng1999@gmail.com>
2021-02-15 19:44:31 -0600

    Heroku setup

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const express = require('express');
 const Stocker = require('./stocker.js');
 const TxtMsg = require('./txtmsg.js');
 const handler = require('./handler.js');
-const credentials = require('./credentials.json');
-const products = require('./products.json')
+const credentials = require('./credentials.json') || process.env;
+const products = require('./limited.json');
 const cart = require('./cart.js');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 let messenger = new TxtMsg(credentials.email, credentials.password)
   .add(credentials.phone, credentials.carrier);
@@ -23,3 +27,9 @@ setInterval(async () => {
     f?.next();
   }
 }, 30000);
+
+app.get('/', (req, res) => {
+  res.send('Online.');
+});
+
+app.listen(PORT);
